4c98b7e079b10284033feac4adf7b80c2f2e283e
Matt Hanley <git@matthanley.co.uk>
2018-06-13 06:53:38 +0100

    Initial public commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM mariadb:latest
+
+COPY galera.cnf /etc/mysql/conf.d/galera.cnf.template
+RUN chown -R mysql:mysql /etc/mysql/conf.d
+
+# Overwrite "mysqld" so we can intercept the
+# call from the upstream entrypoint
+COPY galera.sh /usr/local/sbin/mysqld
+
+ENV MYSQL_ROOT_PASSWORD="mariadb" \
+	MYSQL_USER="mariadb" \
+	MYSQL_PASSWORD="mariadb" \
+	MYSQL_DATABASE="data" \
+	MYSQL_MAX_CONNECTIONS="256" \
+	MYSQL_MAX_STATEMENT_TIME="60" \
+	MYSQL_MAX_ALLOWED_PACKET="16M" \
+	MYSQL_QUERY_CACHE_LIMIT="128K" \
+	MYSQL_QUERY_CACHE_SIZE="128M" \
+	MYSQL_INNODB_BUFFER_POOL_SIZE="1G" \
+	MYSQL_INNODB_LOCK_WAIT_TIMEOUT="60" \
+	MYSQL_INITDB_SKIP_TZINFO="TRUE" \
+	WSREP_FC_FACTOR="0.9" \
+	WSREP_FC_LIMIT="32" \
+	WSREP_SYNC_WAIT="7" \
+	SERVICE_NAME="mariadb" \
+	CLUSTER_NODES="3"
---
+++ galera.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+if echo $* | grep -v -e '--skip-networking' | grep -v -e '--help'; then
+
+  export IP_ADDRESS=`getent hosts | grep $HOSTNAME | awk '{print $1}'`
+
+  echo "Cluster peers:"
+  echo `getent hosts tasks.$SERVICE_NAME`
+
+  if [ -n "$SERVICE_NAME" ]; then
+    CLUSTER_MEMBERS=`getent hosts tasks.$SERVICE_NAME | grep -v $IP_ADDRESS | awk '{print $1}'`
+    # Check we can see enough peers to form a Primary Component
+    if [ `getent hosts tasks.$SERVICE_NAME | wc -l` -lt $(((${CLUSTER_NODES}+1)/2)) ]; then
+      echo "Can't see enough peers to form a cluster; restarting."
+      exit 1
+    fi
+    # If we're the first node then bootstrap the cluster
+    if [ `getent hosts tasks.$SERVICE_NAME | sort -V | head -n 1 | awk '{print $1}'` = $IP_ADDRESS ]; then
+      echo "Looks like we're the first member. Testing for an established cluster between other nodes..."
+      # Check to see if the other nodes have an established cluster
+      for MEMBER in $CLUSTER_MEMBERS
+      do
+        echo "Testing $MEMBER..."
+        if echo "SHOW STATUS LIKE 'wsrep_local_state_comment';" | mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MEMBER | grep "Synced"; then
+          # Connect to existing cluster
+          echo "Success! 😁"
+          export CLUSTER_ADDRESS="gcomm://$MEMBER?pc.wait_prim=yes"
+          break
+        else
+          echo "Failed 😫"
+        fi
+      done
+      # Can't connect to any other hosts; we need to bootstrap
+      if [ -z $CLUSTER_ADDRESS ]; then
+        echo "** No cluster found; bootstrapping on this node **"
+        export CLUSTER_ADDRESS="gcomm://"
+      fi
+    fi
+
+    # Join existing cluster
+    if [ -z $CLUSTER_ADDRESS ]; then
+      # Fetch IPs of service members
+      CLUSTER_MEMBERS=`echo $CLUSTER_MEMBERS | tr ' ' ','`
+      export CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=yes"
+      export MYSQL_PWD="$MYSQL_ROOT_PASSWORD"
+      # Prevent entrypoint trying to (re)create the users
+      unset MYSQL_USER
+      export MYSQL_ROOT_HOST="localhost"
+    fi
+  fi
+
+  echo "Cluster address is $CLUSTER_ADDRESS"
+
+  mv /etc/mysql/conf.d/galera.cnf.template /etc/mysql/conf.d/galera.cnf
+
+  echo "`env`" | while IFS='=' read -r NAME VALUE
+  do
+      sed -i "s#{{${NAME}}}#${VALUE}#g" /etc/mysql/conf.d/galera.cnf
+  done
+
+  chmod 660 /etc/mysql/conf.d/galera.cnf
+
+  echo "Running config:"
+  echo "==============="
+  cat /etc/mysql/conf.d/galera.cnf
+  echo "==============="
+
+fi
+
+exec /usr/sbin/mysqld "$@"
